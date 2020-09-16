aff718e6a2ead2b0302b7a7ed4fa19f1d88fdc63
octod <iamoctod@gmail.com>
2020-09-16 11:58:13 +0200

    chore: adds toasync test

--- tests/applicative.test.ts
+++ tests/applicative.test.ts
@@ -3,7 +3,7 @@ import {
   assertThrows,
 } from "https://deno.land/std/testing/asserts.ts";
 
-import { check, panic } from "../applicative.ts";
+import { check, panic, toasync } from "../applicative.ts";
 
 Deno.test("applicative::" + check.name, () => {
   assertThrows(() => check(false, "errormessage")(123));
@@ -12,4 +12,12 @@ Deno.test("applicative::" + check.name, () => {
 
 Deno.test("applicative::" + panic.name, () => {
   assertThrows(() => panic("message"), Error, "message");
+});
+
+Deno.test("applicative::" + toasync.name, async () => {
+  const fntest = (arg: number) => arg + 1;
+  const fntestasync = toasync(fntest);
+  const arg = 10;
+
+  assertEquals(await fntestasync(arg), fntest(arg));
 });
