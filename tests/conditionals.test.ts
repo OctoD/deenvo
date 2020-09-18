66fdaf0ebd912a9eeedb68c2c6b9c791af946481
octod <iamoctod@gmail.com>
2020-09-18 17:29:09 +0200

    chore: adds test case

--- tests/conditionals.test.ts
+++ tests/conditionals.test.ts
@@ -3,7 +3,9 @@ import {
   assertThrows,
 } from "https://deno.land/std/testing/asserts.ts";
 
+import { bind } from "../applicative.ts";
 import { fi, fifn } from "../conditionals.ts";
+import { reverse } from '../predicate.ts';
 
 Deno.test("Conditionals::" + fi.name, () => {
   assertEquals(fi(true, 0, 1), 0);
@@ -11,6 +13,17 @@ Deno.test("Conditionals::" + fi.name, () => {
 });
 
 Deno.test("Conditionals::" + fifn.name, () => {
+  const fn1 = (arg: number) => arg * 2;
+  const fn2 = (arg: number) => arg / 2;
+  const iseven = (arg: number) => arg % 2 === 0;
+  const isodd = reverse(iseven);
+  const testeven = (arg: number) => fifn(iseven(arg), bind(fn2, arg), bind(fn1, arg));
+  const testodd = (arg: number) => fifn(isodd(arg), bind(fn1, arg), bind(fn2, arg));
+  
   assertEquals(fifn(true, () => 0, () => 1), 0);
   assertEquals(fifn(false, () => 0, () => 1), 1);
+  assertEquals(testeven(10), 5);
+  assertEquals(testeven(11), 22);
+  assertEquals(testodd(10), 5);
+  assertEquals(testodd(11), 22);
 });
